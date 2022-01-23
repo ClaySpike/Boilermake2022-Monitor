62dd2a81a9c016ef9d0452ceca39ae13aec3ee67
ClaySpike <povinellimax@gmail.com>
2022-01-23 02:14:49 -0500

    Interface Updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 import Button from 'react-bootstrap/Button';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Navbar from 'react-bootstrap/Navbar';
@@ -20,6 +21,7 @@ import {
 import { Pie, Line } from "react-chartjs-2";
 
 import { useState, useEffect, useRef } from 'react';
+import logo from './logo.png'
 
 ChartJS.register(
   CategoryScale,
@@ -32,7 +34,7 @@ ChartJS.register(
   Legend
 );
 
-const options = {
+const optionsTemp = {
   responsive: true,
   plugins: {
     legend: {
@@ -40,7 +42,59 @@ const options = {
     },
     title: {
       display: true,
-      text: 'Chart.js Line Chart',
+      text: 'Temperature',
+    },
+  },
+};
+
+const optionsPres = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Pressure',
+    },
+  },
+};
+
+const optionsHumi = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Humidity',
+    },
+  },
+};
+
+const optionsAlti = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Altitude',
+    },
+  },
+};
+
+const optionsSound = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Sound Intensity',
     },
   },
 };
@@ -78,21 +132,17 @@ function addData(chart, label, newData) {
 function App() {
   const [data, setData] = useState(testing);
 
-  const chartRef = useRef(null);
-
-  useEffect(() => {
-    const chart = chartRef.current;
-
-    if (chart) {
-      console.log('ChartJS', chart);
-    }
-  }, []);
+  const tempRef = useRef(null);
+  const humiRef = useRef(null);
+  const presRef = useRef(null);
+  const altiRef = useRef(null);
+  const soundRef = useRef(null);
 
   var counter = 20.0;
 
   useEffect(() => {
     setTimeout(() => {
-      addData(chartRef.current, `${counter}`, counter);
+      addData(tempRef.current, `${counter}`, counter);
     }, 1000);
   }, []);
 
@@ -100,22 +150,27 @@ function App() {
     <>
       <Navbar bg="dark">
         <Container>
-          <Navbar.Brand className="text-light">Testing</Navbar.Brand>
+          <Navbar.Brand className="text-light"><img src={logo} className="App-logo"/> Working Title Lol </Navbar.Brand>
+          <DropdownButton id="dropdown-basic-button" title="Serial Numbers">
+            <Dropdown.Item href="#/action-1">Serial Number 1</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Serial Number 2</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Serial Number 3</Dropdown.Item>
+          </DropdownButton>
         </Container>
       </Navbar>
       <Container>
         <Row className="justify-content-center">
           <Row>
-            <Button>Testing</Button>
-          </Row>
-          <Row>
             <div class="grid-container">
-              <div class="grid-item"><Line ref={chartRef} datasetIdKey='id' data={testing} options={options} /></div>
-              <div class="grid-item"><Line ref={chartRef} datasetIdKey='id' data={testing} options={options} /></div>
+              <div class="grid-item"><Line ref={tempRef} datasetIdKey='id' data={testing} options={optionsTemp} /></div>
+              <div class="grid-item"><Line ref={humiRef} datasetIdKey='id' data={testing} options={optionsHumi} /></div>
             </div>
             <div class="grid-container">
-              <div class="grid-item"><Line ref={chartRef} datasetIdKey='id' data={testing} options={options} /></div>
-              <div class="grid-item"><Line ref={chartRef} datasetIdKey='id' data={testing} options={options} /></div>
+              <div class="grid-item"><Line ref={presRef} datasetIdKey='id' data={testing} options={optionsPres} /></div>
+              <div class="grid-item"><Line ref={altiRef} datasetIdKey='id' data={testing} options={optionsAlti} /></div>
+            </div>
+            <div class="grid-container">
+              <div class="grid-item"><Line ref={soundRef} datasetIdKey='id' data={testing} options={optionsSound} /></div>
             </div>
           </Row>
         </Row>
